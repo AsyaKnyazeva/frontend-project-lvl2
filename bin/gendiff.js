96d158e8b86d4672df332e6cf3523e38989b784c
AsyaKnyazeva <knyazeva478@gmail.com>
2022-06-24 21:26:11 +0300

    Second_step_with mistakes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
-import { Command } from "../node_modules/commander/esm.mjs";
+import { Command } from 'commander/esm.mjs';
+
 const program = new Command();
 program
-.description('Compares two configuration files and shows a difference.')
-.version('output the version number');
+  .description('Compares two configuration files and shows a difference.')
+  .version('output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>', 'path to file 1')
+  .argument('<filepath2>', 'path to file 2')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2, program.opts().format);
+    console.log(diff);
+  });
 
 program.parse();
-

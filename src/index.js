c7214f0cb3353037381bd2b23885a1ed221b18a1
AsyaKnyazeva <knyazeva478@gmail.com>
2022-08-08 16:52:00 +0300

    newBadge

--- src/index.js
+++ src/index.js
@@ -2,31 +2,31 @@ import fs from 'fs';
 import _ from 'lodash';
 
 export const genDiff = (path1, path2) => {
-    const filePath1 = fs.readFileSync(path1, 'utf-8')
-    const filePath2 = fs.readFileSync(path2, 'utf-8')
-    const json1 = JSON.parse(filePath1)
-    const json2 = JSON.parse(filePath2)
-    return compareFiles(json1, json2)
-}
+  const filePath1 = fs.readFileSync(path1, 'utf-8');
+  const filePath2 = fs.readFileSync(path2, 'utf-8');
+  const json1 = JSON.parse(filePath1);
+  const json2 = JSON.parse(filePath2);
+  return compareFiles(json1, json2);
+};
 
 const compareFiles = (data1, data2) => {
-    const keys1 = _.keys(data1)
-    const keys2 = _.keys(data2)
-    const allKeys = _.union(keys1, keys2)
-    const tempArr = [...allKeys]
-    let diffString = ''
-    tempArr.sort().map((key) => {
-      if (!_.has(data1, key)) {
-        return diffString += `  + ${key}: ${data2[key]}\n`
-      }
-      else if (!_.has(data2, key)) {
-        return diffString += `  - ${key}: ${data1[key]}\n`
-      }
-      else if (data1[key] !== data2[key]) {
-        return diffString += `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}\n`
-      }
-      else return diffString += `    ${key}: ${data1[key]}\n`
-    })
-    const result = `{\n${diffString}}`
-    console.log(result)
-  } 
+  const keys1 = _.keys(data1);
+  const keys2 = _.keys(data2);
+  const allKeys = _.union(keys1, keys2);
+  const tempArr = [...allKeys];
+  let diffString = '';
+  tempArr.sort().map((key) => {
+    if (!_.has(data1, key)) {
+      return diffString += `  + ${key}: ${data2[key]}\n`;
+    }
+    if (!_.has(data2, key)) {
+      return diffString += `  - ${key}: ${data1[key]}\n`;
+    }
+    if (data1[key] !== data2[key]) {
+      return diffString += `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}\n`;
+    }
+    return diffString += `    ${key}: ${data1[key]}\n`;
+  });
+  const result = `{\n${diffString}}`;
+  console.log(result);
+};

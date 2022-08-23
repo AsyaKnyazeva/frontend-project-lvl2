a8e6afedf497a7c31a6387f2f739f7b32c04def7
AsyaKnyazeva <knyazeva478@gmail.com>
2022-08-23 16:12:42 +0300

    overright 2308

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
-#!/usr/bin/env node
 import { fileURLToPath } from 'url';
 import * as path from 'path';
-import { gendiff } from '../src/index.js';
+import  gendiff   from '../src/index.js';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,10 +10,19 @@ const getPath = (filename) => path.join(__dirname, '..', '__fixtures__', filenam
 
 const jsonFile1 = getPath('file1.json');
 const jsonFile2 = getPath('file2.json');
-const flatDiffResult = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 const yamlFile1 = getPath('file1.yaml');
 const yamlFile2 = getPath('file2.yaml');
+const testFileStylish = getPath('expectedStylish.txt');
+const expectedStylish= fs.readFileSync(testFileStylish, 'utf-8');
+const testFilePlain = getPath('expectedPlain.txt');
+const expectedPlain= fs.readFileSync(testFilePlain, 'utf-8');
+const testFileJson = getPath('expectedJson.txt');
+const expected = fs.readFileSync(testFileJson, 'utf-8');
 test('gendiff', () => {
-  expect(gendiff(jsonFile1, jsonFile2)).toMatch(flatDiffResult);
-  expect(gendiff(yamlFile1, yamlFile2)).toMatch(flatDiffResult);
+  expect(gendiff(jsonFile1, jsonFile2 )).toEqual(expectedStylish);
+  expect(gendiff(yamlFile1, yamlFile2 )).toEqual(expectedStylish)
+  expect(gendiff(yamlFile1, yamlFile2, 'plain' )).toEqual(expectedPlain);
+  expect(gendiff(jsonFile1, jsonFile2, 'plain' )).toEqual(expectedPlain);
+  expect(gendiff(yamlFile1, yamlFile2, 'json' )).toEqual(expected);
+  expect(gendiff(jsonFile1, jsonFile2, 'json' )).toEqual(expected);
 });
